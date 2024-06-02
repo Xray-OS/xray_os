eafbba32d146f81ea5b29b1e3d27ab00780b4c53
xray_os <xray_os@proton.me>
2024-06-02 08:07:23 -0400

    updating xray-calamares-config-online

--- archiso/airootfs/etc/skel/.local/share/konsole/Profile 1.profile
+++ archiso/airootfs/etc/skel/.local/share/konsole/Profile 1.profile
@@ -1,6 +1,7 @@
 [Appearance]
 ColorScheme=WhiteOnBlack
+Font=MesloLGSNFNerdFont Nerd Font,10,-1,5,75,0,0,0,0,0,Bold
 
 [General]
 Name=Profile 1
-Parent=
+Parent=FALLBACK/
--- archiso/profiledef.sh
+++ archiso/profiledef.sh
@@ -2,10 +2,10 @@
 # shellcheck disable=SC2034
 
 iso_name="xray_os"
-iso_label="XRAY_$(date +%Y%m)"
-iso_publisher="Xray_OS <https://gitlab.com/Xray_OS>"
+iso_label="xray_os-v24.06.03"
+iso_publisher="Xray_OS <http://www.arcolinux.info>"
 iso_application="Xray_OS Live/Rescue CD"
-iso_version="$(date +%Y.%m.%d)"
+iso_version="v24.06.03"
 install_dir="arch"
 buildmodes=('iso')
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
@@ -15,21 +15,15 @@ arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
 file_permissions=(
   ["/etc/gshadow"]="0:0:600"
   ["/etc/shadow"]="0:0:600"
   ["/root"]="0:0:700"
-  ["/root/.automated_script.sh"]="0:0:755"
-# ["/etc/polkit-1/rules.d"]="0:0:750"
-  ["/etc/sudoers.d"]="0:0:750"
-  ["/usr/local/bin/xray-make-a-pure-arch"]="0:0:755"
-  ["/usr/local/bin/xray-displaymanager-check"]="0:0:755"
-  ["/usr/local/bin/fix-keys"]="0:0:755"
-  ["/usr/local/bin/ucode"]="0:0:755"
-  ["/usr/local/bin/skel"]="0:0:755"
-  ["/etc/grub.d/40_custom"]="0:0:755"
   ["/root/.gnupg"]="0:0:700"
-  ["/usr/local/bin/choose-mirror"]="0:0:755"
-  ["/usr/local/bin/Installation_guide"]="0:0:755"
-  ["/usr/local/bin/livecd-sound"]="0:0:755"
+  ["/root/.automated_script.sh"]="0:0:755"
+  ["/etc/polkit-1/rules.d"]="0:0:750"
+  ["/etc/sudoers.d"]="0:0:750"
+  ["/etc/grub.d/40_custom"]="0:0:755"
+  ["/usr/local/bin/xray-snapper"]="0:0:755"
 )
